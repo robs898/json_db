de01ccfb0f07310b4718dce1505c0011e73c76f6
Robbie Mackenzie <rmackenzie8@gmail.com>
2019-05-17 20:44:17 +0100

    get is goooood

--- server2.go
+++ server2.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
-	"io/ioutil"
 )
 
 type Birthday struct {
@@ -18,60 +18,71 @@ func getUser(w http.ResponseWriter, r *http.Request) string {
 	var validPath = regexp.MustCompile("^/([a-zA-Z0-9]+)$")
 	m := validPath.FindStringSubmatch(r.URL.Path)
 	if m == nil {
-		http.Error(w, "Invalid user", 400)
 		return ""
 	}
 	return m[1]
 }
 
-func getDb(w http.ResponseWriter, user string, ignoreError bool) Birthday {
+func getDb(w http.ResponseWriter, user string) (Birthday, error) {
 	var db Birthday
 	filename := "data/" + user + ".json"
 	byteFile, err := ioutil.ReadFile(filename)
 	if err != nil {
-		if ! ignoreError {
-			http.Error(w, "User db not found", 404)
-		}
-		return db
+		return db, err
 	}
 	err2 := json.Unmarshal(byteFile, &db)
 	if err2 != nil {
-		http.Error(w, "User db invalid json", 500)
-		return db
+		return db, err2
 	}
-	return db
+	return db, nil
 }
 
-func parseData() {
+func parseData(w http.ResponseWriter, r *http.Request) (Birthday, error) {
 	decoder := json.NewDecoder(r.Body)
-	        var birthday Birthday
-		        err := decoder.Decode(&birthday)
+	var birthday Birthday
+	err := decoder.Decode(&birthday)
+	if err != nil {
+		return birthday, err
+	}
+	return birthday, nil
 }
 
-func createDb(bday Birthday, user string) {
+func createDb(bday Birthday, user string) error {
 	filename := "data/" + user + ".json"
 	json, _ := json.Marshal(bday)
 	return ioutil.WriteFile(filename, json, 0600)
 }
 
 func mainHandle(w http.ResponseWriter, r *http.Request) {
+	log.Println("INFO", r.Method, r.URL.Path)
 	if r.Method == "POST" {
 		user := getUser(w, r)
-		log.Println("Current user:", user)
-		bday := getDb(w, user, true)
-		log.Println(bday)
-
+		db, err := getDb(w, user)
+		if err != nil {
+			log.Println("INFO no existing db for user", user)
+		}
+		log.Println(db)
+		data, err2 := parseData(w, r)
+		if err2 != nil {
+			log.Println("ERROR invalid JSON", data)
+			http.Error(w, "invalid JSON", 400)
+		}
+		log.Println(data)
 	} else if r.Method == "GET" {
 		user := getUser(w, r)
-		log.Println(r.Method)
-		bday := getDb(w, user, false)
-		log.Println(bday)
+		if user == "" {
+			log.Println("ERROR invalid user", user)
+			http.Error(w, "invalid user", 400)
+		} else {
+			db, err := getDb(w, user)
+			if err != nil {
+				log.Println("ERROR no db found for", user)
+				http.Error(w, "no found db for user", 404)
+			} else {
+				json.NewEncoder(w).Encode(db)
+			}
+		}
 	}
-	//birthdays := Birthdays{
-	//	Birthday{Name: "dave"},
-	//	Birthday{Name: "bob"},
-	//}
-	//json.NewEncoder(w).Encode(birthdays)
 }
 
 func main() {
